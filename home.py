1aca0fbc16a9c5d22c8cc5aa93f226a8c721c0fc
Susanna Mostaghim <36485111+ssmostagh@users.noreply.github.com>
2025-08-04 11:03:53 -0400

    Add files via upload

---
+++ home.py
@@ -0,0 +1,27 @@
+import streamlit as st #pip install streamlit
+import requests
+import os
+import queue
+import io
+import time
+#from PIL import Image
+#import PIL.Image
+from streamlit.runtime.scriptrunner import get_script_run_ctx #for multi-threading in streamlit
+from streamlit.runtime.scriptrunner import add_script_run_ctx #for multi-threading in streamlit
+from google import genai
+import threading
+
+#keyload()
+
+#Streamlit Styling
+st.set_page_config(layout="wide")
+
+with st.container(border=False):
+    st.title("Moodboards")
+    #st.markdown("Select from the menu.")
+    st.markdown("""
+                #### 1) Please note that these are rapid prototypes.
+                #### 2) The prototypes leverage prompts to adjust how Gemini answers questions.
+                #### 3) A full implementation can use grounding and custom trained models.""")
+    #current_folder = os.path.basename(os.getcwd())
+    st.markdown(f"Last update: 07.2025")
